97bd211fae3a0089c3c501b3c117a078fe9cddbc
Francisco-AMenezes <franciscoalvesmenezes90@gmail.com>
2024-08-17 22:10:08 -0300

    resgatando os valores do form

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,16 +1,16 @@
-document.querySelector("form").addEventListener("submit", function (event) {
-  event.preventDefault(); // previne o envio do formulário para fins de teste
+function sendWhatsappMessage(event) {
+    event.preventDefault();
 
-  const phone = document.getElementById("phone").value;
-  const cep = document.getElementById("cep").value;
-  const whatsapp = document.getElementById("whatsapp").value;
-  const documentNumber = document.getElementById("document").value;
+    // coleta os valores do formulário
 
-  // remove qualquer caracter não numérico
-  const phoneNumber = Number(phone.replace(/\D/g, ""));
-  const cepNumber = Number(cep.replace(/\D/g, ""));
-  const documentNumberClean = Number(documentNumber.replace(/\D/g, ""));
-  const whatsappNumber = Number(whatsapp.replace(/\D/g, ""));
-
-  console.log(phoneNumber, cepNumber, documentNumberClean, whatsappNumber);
-});
+    const name = document.getElementById('name');
+    const documentNumber = document.getElementById('document')
+    const ie = document.getElementById('ie')
+    const addres = document.getElementById('addres')
+    const cep = document.getElementById('cep')
+    const neighborhood = document.getElementById('neighborhood')
+    const city = document.getElementById('city')
+    const uf = document.getElementById('uf')
+    const email = document.getElementById('email')
+    
+}
